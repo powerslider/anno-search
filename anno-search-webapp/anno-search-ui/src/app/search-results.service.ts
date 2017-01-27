8b65c7b6a75c58cbcfea9dfd1404b734522bc95e
Aleksandar Savev <savevjr@gmail.com>
2017-01-27 15:48:14 +0200

    Add requests to the API

--- anno-search-webapp/anno-search-ui/src/app/search-results.service.ts
+++ anno-search-webapp/anno-search-ui/src/app/search-results.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http, Response, Headers, RequestOptionsArgs } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { Observer } from 'rxjs/Observer';
 import { Result } from './model/result';
@@ -11,7 +11,7 @@ export class SearchResultsService {
 
   results: AnnoDocument[];
   resultsAuto: Entity[];
-  serverUrl: string;
+  serverUrl: string = "http://localhost:8089/";
   showSuggestions = false;
   private searchText: string;
   suggestion: Entity;
@@ -20,52 +20,36 @@ export class SearchResultsService {
 
   public setSearchText(searchText: string) {
     this.searchText = searchText;
-    this.getMockResults(this.searchText);
+    this.fetchSuggestions(searchText);
   }
 
   public setSuggestion(suggestion: Entity) {
     this.suggestion = suggestion;
-    this.getMockResults2(suggestion);
+    this.fetchResults(suggestion);
   }
 
-  private getMockResults(filter: string) {
-    let mock: Entity[] = [];
-    mock.push({ title: "Barack Obama", data: ["president", "person", "black"] });
-    mock.push({ title: "Donald Trump", data: ["president", "person", "white"] });
-    mock.push({ title: "Google", data: ["Organization"] });
-    mock.push({ title: "blog", data: ["Work"] });
-    if (filter != null && filter.length > 0) {
-      mock = mock.filter(x => x.title.toLowerCase().indexOf(filter) != -1);
-      this.showSuggestions = true;
-    } else {
-      mock = [];
-      this.showSuggestions = false;
-    }
-    setTimeout(() => {
-      this.resultsAuto = mock;
-      this.results = [];
-    }, 1234);
-  }
-
-  private getMockResults2(filter: Entity) {
-    let mock: AnnoDocument[] = [];
-    mock.push({
-      text: "This is a text.\nAnd this is another text.",
-      title: "This is the only document",
-      annotations: [{ type: "ivan", start: 5, end: 6 },
-      { type: "peter", start: 8, end: 8 }]
-    });
-
-    setTimeout(() => {
-      this.results = mock;
-    }, 1234);
-  }
-
-  fetchAutoResults(filter: string) {
-    this.getMockResults(filter);
+  fetchSuggestions(filter: string): void {
+    this.http.get(this.serverUrl + "api/search/annotations?string=" + filter)
+      .map(this.extractSuggestions).subscribe(x => { this.resultsAuto = x; this.showSuggestions = true; });
   }
 
   fetchResults(filter: Entity) {
-    this.getMockResults2(filter);
+    this.http.get(this.serverUrl + "api/search/docs?string=" + filter.title + "&type=" + filter.type)
+      .map(this.extractResults).subscribe(x => { this.results = x; this.showSuggestions = false; });
+  }
+
+  extractSuggestions(res: Response) {
+    let arr: Array<any> = res.json();
+    return arr.slice(0, 10).map((xx): Entity => {
+      return { title: Object.keys(xx)[0], type: xx[Object.keys(xx)[0]] };
+    });
+  }
+
+  extractResults(res: Response) {
+    let arr = res.json();
+    console.log(arr);
+    return arr.map((xx): Result => {
+      return { title: xx["title"], matchedText: xx["matchedText"].replace(/(?:\r\n|\r|\n)/g, '<br />'), id: xx["id"], sentimentScore: xx["sentimentScore"] };
+    });
   }
 }
