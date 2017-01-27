650ed2821a70adc0b4eaf1af435056f6d6a47c6a
Aleksandar Savev <savevjr@gmail.com>
2017-01-27 21:32:32 +0200

    Add tooltip to each annotation

--- anno-search-webapp/anno-search-ui/src/app/color-paragraph.pipe.ts
+++ anno-search-webapp/anno-search-ui/src/app/color-paragraph.pipe.ts
@@ -2,12 +2,17 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { AnnoDocument } from './model/annodocument';
 import { AnnotationInfo } from './model/annotation';
 
+import { BrowserModule, DomSanitizer, SafeHtml } from '@angular/platform-browser'
+
 @Pipe({
   name: 'colorParagraph'
 })
 export class ColorParagraphPipe implements PipeTransform {
 
-  transform(value: AnnoDocument, args?: any): string {
+  constructor(private _sanitizer: DomSanitizer) { }
+
+
+  transform(value: AnnoDocument, args?: any): SafeHtml {
     if (value.annotations == null || value.annotations.length == 0) {
       return "";
     }
@@ -28,21 +33,28 @@ export class ColorParagraphPipe implements PipeTransform {
         lastPushed = anno.start;
       }
       let p: string = value.text.substring(anno.start, anno.end);
-      result.push(this.wrap(p, anno.string + " : " + anno.type));
+      result.push(this.wrap(p, anno));
       lastPushed = anno.end - 1;
     }
     if (lastPushed + 1 < value.text.length) {
       result.push(value.text.substring(lastPushed + 1));
     }
-    return result.join("").replace(/(?:\r\n|\r|\n)/g, '<br />');
+    return this._sanitizer.bypassSecurityTrustHtml(result.join("").replace(/(?:\r\n|\r|\n)/g, '<br />'));
   }
 
   private colors: number = 5;
   private usedColors: number = 0;
 
-  private wrap(value: string, title?: string): string {
-    title = title != null ? "title=\"" + title + "\"" : "";
-    let clas: string = "anno" + (this.usedColors++ % this.colors);
-    return "<span class=\"anno " + clas + "\"" + title + ">" + value + "</span>";
+  private wrap(value: string, anno: AnnotationInfo): string {
+    let colorCode: number = (this.usedColors++ % this.colors) + 1;
+    let clas: string = "anno" + colorCode;
+    return "<span class=\"anno " + clas + " tooltip\">" + this.gen(anno.string, anno.type, colorCode) + value + "</span>";
+  }
+
+  private gen(s: string, type: string, colorCode: number): string {
+    return "<div class=\"tooltiptext tooltip-color" + colorCode + "\">" +
+      "<span>Keyword: " + s + "</span><br/>" +
+      "<span>Type: " + type + "</span>" +
+      "</div>";
   }
 }
